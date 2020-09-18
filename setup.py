b6a4204f9f725f4f1a8e4c5f6a86363b48736275
Yuxiang Yang <yxyang@berkeley.edu>
2020-09-18 16:12:24 -0700

    modified setup.py to include correct packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="locomotion_simulation",
       version="0.0.1",
-      packages=["locomotion"],
+      packages=find_packages(),
       install_requires=["gym", "pybullet", "numpy", "scipy"])
